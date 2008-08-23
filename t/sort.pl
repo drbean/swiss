d9a6ad29dbc87adc5a39a03013382d21a0e3f3e9
drbean <drbean@users.noreply.github.com>
2008-08-23 07:17:20 +0000

    r7@localhost:  drbean | 2008-08-23 18:08:53 +0800  Replace my perm routines with HOP's permutation iterator. Make sort routine an anon sub

--- t/sort.pl
+++ t/sort.pl
@@ -1,46 +1,71 @@
-#perl
+#!perl
 
 use strict;
 use warnings;
 
-# my @words = qw(1.5 1.5Remainder 1 1Remainder 1Bye);
-my @words = qw(A B C D);
+sub permute {
+  my @items = @_;
+  my $n = 0;
+  return sub {
+    $n++, return @items if $n==0;
+    my $i;
+    my $p = $n;
+    for ($i=1; $i<=@items && $p%$i==0; $i++) {
+      $p /= $i;
+    }
+    my $d = $p % $i;
+    my $j = @items - $i;
+    return if $j < 0;
 
-# my @ongoingperms = ( ['A'] );
-my @ongoingperms;
-foreach my $n ( 0 .. $#words )
-{
-	# my @ongoingperms ||= ();
-	my @perms;
-	do
-	{
-		my $perm = shift @ongoingperms;
-		my @perm = @ongoingperms? @$perm: ();
-		my $last = @perm? $#perm: 0;
-		foreach my $pos ( 0 .. $last )
-		{
-			my @forextended;
-			@forextended[0..$pos-1] = @perm[0..$pos-1];
-			$forextended[$pos] = $words[$n];
-			foreach my $other ( $pos+1 .. $#perm )
-			{
-				last unless defined $perm[$other];
-				$forextended[$other+1] = $perm[$other];
-			}
-			push @perms, \@forextended;
-			my @aftextended;
-			@aftextended[0..$pos] = @perm[0..$pos];
-			$aftextended[$pos+1] = $words[$n];
-			foreach my $other ( $pos+1 .. $#perm )
-			{
-				last unless defined $perm[$other];
-				$aftextended[$other+1] = $perm[$other];
-			}
-			push @perms, \@aftextended;
-		}
-	} while @ongoingperms;
-	# print @$_ , "\t" for @perms;
-	@ongoingperms = @perms;
-	# print "\n";
+    @items[$j+1..$#items] = reverse @items[$j+1..$#items];
+    @items[$j,$j+$d] = @items[$j+$d,$j];
+
+    $n++;
+    return @items;
+  };
 }
-print @$_ , "\t" for @ongoingperms;
+
+my $it = permute(qw/1.5 1.5Remainder 1 1Remainder 1Bye/);
+
+my $groupsort = sub {
+       if ( $a =~ /Bye$/ ) {
+	   return -1;
+       }
+       elsif ( $b =~ /Bye$/ ) {
+	   return 1;
+       }
+       elsif ( $a =~ /^(\d+\.?5?)Remainder$/ ) {
+	   if ( $1 eq $b ) {
+	       return -1;
+	   }
+	   else {
+	       my $numbers_a = $1;
+	       if ( $b =~ /^(\d+\.?5?)Remainder$/ ) {
+		   return $numbers_a <=> $1;
+	       }
+	       else {
+		   return $numbers_a <=> $b;
+	       }
+	   }
+       }
+       elsif ( $b =~ /^(\d+\.?5?)Remainder$/ ) {
+	   if ( $1 eq $a ) {
+	       return 1;
+	   }
+	   else {
+	       my $numbers_b = $1;
+	       if ( $a =~ /^(\d+\.?5?)Remainder$/ ) {
+		   return $1 <=> $numbers_b;
+	       }
+	       else {
+		   return $a <=> $numbers_b;
+	       }
+	   }
+       }
+       else { return $a cmp $b; }
+};
+
+while (my @order = &$it )
+{
+	print sort $groupsort @order , "\n";
+}
