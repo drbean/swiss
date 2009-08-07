6850ee9ca193d2a0087154fcca277579860769a6
drbean <drbean@users.noreply.github.com>
2009-08-07 14:35:55 +0000

    r414@momotaro:  greg | 2009-08-05 22:17:33 +0900  add_player, edit_players, rounds, pair, working. But byed player not showing up.

--- web/lib/Swiss/Controller/Root.pm
+++ web/lib/Swiss/Controller/Root.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
+use List::MoreUtils qw/all notall/;
+
 #
 # Sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
@@ -66,38 +68,110 @@ sub name : Local {
 }
 
 
-=head2 players
+=head2 add_player
 
 First round, players, number of rounds
 
 =cut
 
-sub players : Local {
+sub add_player : Local {
         my ($self, $c) = @_;
-	$c->stash->{tournament} = $c->request->cookie('tournament')->value;
-	my %cookies = ( id => undef, name => undef, rating => undef );
-	for my $key ( qw/id name rating/ ) {
-		if ( defined $c->request->cookie($key) ) {
-			my @cookies = $c->request->cookie($key)->value;
-			$cookies{$key} = \@cookies;
-		}
+	my $cookies = $c->request->cookies;
+	my $tourney = $c->request->cookie('tournament')->value;
+	my @playerlist = $c->model('GTS')->turnIntoPlayers($tourney, $cookies);
+	$c->stash->{tournament} = $tourney;
+	if (  $c->request->params->{stop} ) {
+		$c->stash->{template} = 'rounds.tt2';
+		return;
 	}
-	my $playerN = ref $cookies{id} eq 'ARRAY'? @{ $cookies{id} }: 0;
-	my @playerlist = map { { id => $cookies{id}->[$_],
-				name => $cookies{name}->[$_],
-				rating => $cookies{rating}->[$_] } }
-				0 .. $playerN-1;
 	my %entrant = map { $_ => $c->request->params->{$_} }
 							qw/id name rating/;
-
-	for my $key ( qw/id name rating/ ) {
-		my $cookie = $cookies{$key};
-		push @$cookie, $entrant{$key};
-		$c->response->cookies->{$key} = { value => $cookie };
+	my $mess;
+	if ( $mess = $c->model('GTS')->allFieldCheck( \%entrant ) ) {
+		$c->stash->{error_msg} = $mess;
+		$c->stash->{playerlist} = \@playerlist;
 	}
-	push @playerlist, \%entrant;
-$DB::single=1;
-	$c->stash->{playerlist} = \@playerlist;
+	elsif ( $mess = $c->model('GTS')->idDupe(@playerlist, \%entrant ) ) {
+		$c->stash->{error_msg} = $mess;
+		$c->stash->{playerlist} = \@playerlist;
+	}
+	else {
+		push @playerlist, \%entrant;
+		$c->stash->{playerlist} = \@playerlist;
+		my %cookies = $c->model('GTS')->turnIntoCookies(
+			$tourney, \@playerlist);
+		$c->response->cookies->{$_} = { value => $cookies{$_ } }
+			for keys %cookies;
+	}
+	$c->stash->{template} = 'players.tt2';
+}
+
+
+
+=head2 edit_players
+
+First round, players, number of rounds
+
+=cut
+
+sub edit_players : Local {
+        my ($self, $c) = @_;
+	my $cookies = $c->request->cookies;
+	my $tourney = $c->request->cookie('tournament')->value;
+	my $newlist = $c->request->params->{playerlist};
+	my @playerlist = $c->model('GTS')->parsePlayers($tourney, $newlist);
+	$c->stash->{template} = 'players.tt2';
+	my $mess;
+	if ( $mess = $c->model('GTS')->allFieldCheck(@playerlist ) ) {
+		$c->stash->{error_msg} = $mess;
+		$c->stash->{playerlist} = \@playerlist;
+	}
+	elsif ( $mess = $c->model('GTS')->idDupe(@playerlist ) ) {
+		$c->stash->{error_msg} = $mess;
+		$c->stash->{playerlist} = \@playerlist;
+	}
+	else {
+		my %cookies = $c->model('GTS')->turnIntoCookies(
+			$tourney, \@playerlist);
+		$c->response->cookies->{$_} = { value => $cookies{$_ } }
+			for keys %cookies;
+		$c->stash->{playerlist} = \@playerlist;
+	}
+}
+
+
+=head2 rounds
+
+Number of rounds
+
+=cut
+
+sub rounds : Local {
+        my ($self, $c) = @_;
+	my $tourney = $c->request->cookie('tournament')->value;
+	my $rounds = $c->request->params->{rounds};
+	$c->response->cookies->{"${tourney}_rounds"} = { value => $rounds };
+	$c->stash->{rounds} = $rounds;
+	$c->detach('pair');
+}
+
+
+=head2 pair
+
+Pair first round
+
+=cut
+
+sub pair : Local {
+        my ($self, $c) = @_;
+	my $cookies = $c->request->cookies;
+	my $tourney = $c->request->cookie('tournament')->value;
+	my @playerlist = $c->model('GTS')->turnIntoPlayers($tourney, $cookies);
+	my $rounds = $c->stash->{"${tourney}_rounds"};
+	my @games = $c->model('GTS')->pair( {rounds => $rounds, entrants => \@playerlist} );
+	$c->stash->{roles} = $c->model('GTS')->roles;
+	$c->stash->{games} = \@games;
+	$c->stash->{template} = "draw.tt2";
 }
 
 
