c93cb4fd6959ef9d63029ff66b97d9f872ab0e91
drbean <drbean@users.noreply.github.com>
2009-12-21 08:21:57 +0000

    r2330@e_learning:  drbean | 2009-10-19 18:27:35 +0800  Arbiters has_many Tournaments and might_have Tournament

--- web/lib/Swiss/Schema/Result/Arbiters.pm
+++ web/lib/Swiss/Schema/Result/Arbiters.pm
@@ -36,6 +36,10 @@ __PACKAGE__->set_primary_key("id");
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:d57/qeBCjmLw5JdoWCEnNw
 
+__PACKAGE__->has_many( tournaments => 'Swiss::Schema::Result::Tournaments', 
+	'arbiter' );
+__PACKAGE__->might_have( tournament => 'Swiss::Schema::Result::Tournament', 
+	'arbiter' );
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
