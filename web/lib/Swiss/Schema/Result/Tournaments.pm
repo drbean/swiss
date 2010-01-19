40ed69c31ce889be7aec5dd5fad47e1b3bb2359c
drbean <drbean@users.noreply.github.com>
2010-01-19 10:27:23 +0000

    r3956@sac:  drbean | 2010-01-01 14:38:53 +0800   r2272@e_learning:  drbean | 2009-10-05 12:27:01 +0800   id, description, arbiter, columns added to name and rounds, with id and arbiter as PK

--- web/lib/Swiss/Schema/Result/Tournaments.pm
+++ web/lib/Swiss/Schema/Result/Tournaments.pm
@@ -8,7 +8,28 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("InflateColumn::DateTime", "Core");
 __PACKAGE__->table("tournaments");
 __PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 15,
+  },
   "name",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 15,
+  },
+  "description",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 100,
+  },
+  "arbiter",
   {
     data_type => "VARCHAR",
     default_value => undef,
@@ -23,7 +44,7 @@ __PACKAGE__->add_columns(
     size => undef,
   },
 );
-__PACKAGE__->set_primary_key("name", );
+__PACKAGE__->set_primary_key("id", "arbiter");
 
 
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
