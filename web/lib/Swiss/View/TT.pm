78cd95e3e12b2557f963291800931e3f28d7e265
drbean <drbean@users.noreply.github.com>
2010-01-22 05:27:06 +0000

    r370@momotaro:  greg | 2009-08-01 19:16:21 +0900  Catalyst 'Swiss' app in web, subdir of swiss. Different than Beans, which was at top dir, I think

--- web/lib/Swiss/View/TT.pm
+++ web/lib/Swiss/View/TT.pm
@@ -2,28 +2,6 @@ package Swiss::View::TT;
 
 use strict;
 use base 'Catalyst::View::TT';
-use Template::Stash;
-
-$Template::Stash::SCALAR_OPS->{lc} = sub {
-	my ($string ) = @_;
-	return lc $string;
-};
-
-$Template::Stash::LIST_OPS->{abbrevs} = sub {
-	my ( $list, @roles ) = @_;
-	return [ map { ( $_ eq $roles[0] or $_ eq $roles[1] ) ?
-			substr( $_, 0, 1 ) : '-' } @$list ];
-};
-
-$Template::Stash::LIST_OPS->{substrs} = sub {
-	my ($list, $offset, $length ) = @_;
-	return [ map { substr( $_, $offset, $length ) } @$list ];
-};
-
-$Template::Stash::LIST_OPS->{map} = sub {
-	my ($list, $sub ) = @_;
-	return [ map { $sub->($_) } @$list ];
-};
 
 __PACKAGE__->config(
 	# Change default TT extension
@@ -32,11 +10,6 @@ __PACKAGE__->config(
 	INCLUDE_PATH => [
 	       Swiss->path_to( 'root/src' ),
 	   ],
-	# Set to 1 for detailed timer stats in your HTML as comments
-	TIMER              => 0,
-	# This is your wrapper template located in the 'root/src'
-	WRAPPER => 'wrapper.tt2',
- 
 );
 
 =head1 NAME
