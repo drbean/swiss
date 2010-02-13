3aa6159ca5daf0a2d625b14562389883ccf4783e
drbean <drbean@users.noreply.github.com>
2010-02-13 03:21:03 +0000

    r1948@momotaro:  greg | 2010-02-11 21:31:12 +0900  Ratings belong to Players. Players have many Ratings over rounds (after a round) of tournaments (usually sequential but perhaps concurrent.) So PK is player, round, tournament.

---
+++ web/lib/Swiss/Schema/Result/Ratings.pm
@@ -0,0 +1,44 @@
+package Swiss::Schema::Result::Scores;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Core");
+__PACKAGE__->table("scores");
+__PACKAGE__->add_columns(
+  "tournament",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 15,
+  },
+  "player",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 10,
+  },
+  "score",
+  {
+    data_type => "TINYINT",
+    default_value => 0,
+    is_nullable => 0,
+    size => undef,
+  },
+);
+__PACKAGE__->set_primary_key("tournament", "player");
+
+
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gJHWIeNlHGuuGlCZHyMJSQ
+
+__PACKAGE__->belongs_to(
+	tournament => 'Swiss::Schema::Result::Tournaments', 'tournament' );
+__PACKAGE__->belongs_to( ided =>'Swiss::Schema::Result::Players', 'player' );
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
