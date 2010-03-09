0c1023bc66ed77bc2df01a9004ee255a5b10030a
drbean <drbean@users.noreply.github.com>
2010-03-09 13:57:30 +0000

    r4496@sac:  drbean | 2010-03-09 09:24:18 +0800  -l tournament/league

--- web/script_files/updateplayers.pl
+++ web/script_files/updateplayers.pl
@@ -38,6 +38,7 @@ use Grades;
 use Config::General;
 
 my $script = Grades::Script->new_with_options;
+my $tournament = $script->league;
 my $round = $script->round;
 
 my @MyAppConf = glob( "$Bin/../*.conf" );
@@ -58,26 +59,24 @@ my $ratings = $d->resultset('Ratings');
 my $members = $d->resultset('Members');
 my (%players, @members, @ratings);
 my $leagues = $script->league;
-for my $tournament ( qw/GL00012 MIA0009 BMA0077 BMA0076 FLA0031 GL00027 FLA0018/) {
-	my $league = League->new( id =>
-		"$config{leagues}/$tournament" );
-	my $members = $league->members;
-	foreach my $member ( @$members ) {
-		my $id = $member->{id};
-		unless ( defined $players{$id} ) { 
-		$players{$id} = {
-			name => $member->{name},
-			id => $member->{id},
-			};
-		push @members, {
-			tournament => $tournament, player => $member->{id},
-			absent => 'False', firstround => $round };
-		push @ratings, { 
-				player => $member->{id},
-				tournament => $tournament,
-				round => ($round - 1),
-				value => $member->{rating} || 0 };
-		}
+my $league = League->new( id =>
+	"$config{leagues}/$tournament" );
+my $filemembers = $league->members;
+foreach my $member ( @$filemembers ) {
+	my $id = $member->{id};
+	unless ( defined $players{$id} ) { 
+	$players{$id} = {
+		name => $member->{name},
+		id => $member->{id},
+		};
+	push @members, {
+		tournament => $tournament, player => $member->{id},
+		absent => 'False', firstround => $round };
+	push @ratings, { 
+			player => $member->{id},
+			tournament => $tournament,
+			round => ($round - 1),
+			value => $member->{rating} || 0 };
 	}
 }
 my @players = values %players;
