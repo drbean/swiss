b87adef34fd887370d3cd25fd584f7f8fbaa6ea0
drbean <drbean@users.noreply.github.com>
2010-04-03 09:51:51 +0000

    r5594@sac:  drbean | 2010-04-03 17:52:54 +0800  short stage differentiators, p, px after league id

--- web/script_files/pairseat.pl
+++ web/script_files/pairseat.pl
@@ -169,9 +169,10 @@ sub run {
 			my $web = Net::FTP->new( 'web.nuu.edu.tw' ) or warn "web.nuu?";
 			$web->login("greg", "1514") or warn "login: greg?";
 			$web->cwd( 'public_html' ) or die "No cwd to public_html,";
+			my $page = $stage eq 'pair'? "${league}p.html": "${league}px.html";
 			$web->put(
-				"$::config{leagues}/$league/comp/${stage}seat.$filetype",
-				"$league$stage.html") or die "put file?";
+				"$::config{leagues}/$league/comp/${stage}seat.$filetype", $page 
+				) or die "put file?";
 		}
 	}
 }
