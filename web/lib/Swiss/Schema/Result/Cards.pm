0d950e64e2f1cca692443ab67d839814f1ceea99
drbean <drbean@users.noreply.github.com>
2010-08-10 04:14:57 +0000

    Cards schema with tournament, round, table PK. Will have to create table according to F1.

---
+++ web/lib/Swiss/Schema/Result/Cards.pm
@@ -0,0 +1,55 @@
+package Swiss::Schema::Result::Cards;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Core");
+__PACKAGE__->table("cards");
+__PACKAGE__->add_columns(
+  "tournament",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 15,
+  },
+  "round",
+  {
+    data_type => "TINYINT",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "table",
+  {
+    data_type => "TINYINT",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "white",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 10,
+  },
+  "black",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 10,
+  },
+);
+__PACKAGE__->set_primary_key("tournament", "round", "table");
+
+
+# Not Created by DBIx::Class::Schema::Loader
+
+__PACKAGE__->belongs_to(
+	tournament => 'Swiss::Schema::Result::Tournaments', 'tournament' );
+
+1;
