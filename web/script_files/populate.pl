830d4927ad7521701ad36cd89dc398af366d5950
drbean <drbean@users.noreply.github.com>
2011-02-28 07:35:21 +0000

    Join populateplayers, populatetournaments into 1, to avoid postgresql's foreign key constraint errors.

---
+++ web/script_files/populate.pl
@@ -0,0 +1,118 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+populateplayers.pl - Enter players in database via script
+
+=head1 SYNOPSIS
+
+populateplayers.pl
+
+=head1 DESCRIPTION
+
+Enter players, tournaments in database via script. TODO Include the nonentity, Bye player, who makes a pair when there are an odd number of players. Problem is how to prevent a partner being looked for for that Bye player.
+
+=head1 AUTHOR
+
+Dr Bean
+
+=head1 COPYRIGHT
+
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+
+use IO::All;
+my $io = io '-';
+
+use Grades;
+
+use Config::General;
+
+my $script = Grades::Script->new_with_options;
+
+my @MyAppConf = glob( "$Bin/../*.conf" );
+die "Which of @MyAppConf is the configuration file?"
+			unless @MyAppConf == 1;
+my %config = Config::General->new($MyAppConf[0])->getall;
+my $name = $config{name};
+require $name . ".pm";
+my $model = "${name}::Schema";
+my $modelfile = "$name/Model/DB.pm";
+my $modelmodule = "${name}::Model::DB";
+# require $modelfile;
+
+my $connect_info = $modelmodule->config->{connect_info};
+my $d = $model->connect( @$connect_info );
+
+my @officials = ( [ qw/id name password/ ] );
+push @officials, [split] for <<OFFICIALS =~ m/^.*$/gm;
+193001	DrBean	ok
+greg	greg	ok
+OFFICIALS
+$d->resultset('Arbiters')->populate( \@officials );
+
+my $roundset = $d->resultset('Round');
+my (@startingrounds, %players, @members);
+for my $tournament ( qw/FIA0038 BMA0033 FLA0016 FLA0030 MIA0012 FLA0021 GL00022 GL00005/ ) {
+	my $league = League->new( leagues => $config{leagues}, id => $tournament );
+	my $members = $league->members;
+	my $name = $league->name;
+	my $description = $league->field;
+	my $arbiter = '193001';
+	my $rounds = 18;
+	my $firstround = { value => 0, tournament => $tournament };
+	$d->resultset('Tournaments')->find_or_create( {
+			name => $name,
+			id => $tournament,
+			description => $description,
+			arbiter => $arbiter,
+			rounds => $rounds,
+		} );
+	my $round = $roundset->find({ tournament => $tournament });
+	$round = $round? $round->value: 0;
+	push @startingrounds, { tournament => $tournament, value => $round };
+	foreach my $member ( @$members ) {
+		my $id = $member->{id};
+		push @members, { player => $id, tournament => $tournament,
+							absent => 'False', firstround => $round };
+		unless ( defined $players{$id} ) { 
+			$players{$id} = {
+				name => $member->{name},
+				id => $id,
+				rating => [ { 
+					player => $member->{id},
+					tournament => $tournament,
+					round => 0,
+					value => $member->{rating} || 0 } ]
+			};
+		}
+		else {
+			push @{ $players{$id}->{rating} }, 
+				{ 
+					player => $member->{id},
+					tournament => $tournament,
+					round => 0,
+					value => $member->{rating} || 0 };
+		}
+	}
+}
+my @players = values %players;
+find_or_populate( 'Players', \@players );
+find_or_populate( 'Round', \@startingrounds );
+find_or_populate( 'Members', \@members );
+
+sub find_or_populate
+{
+    my $class = $d->resultset(shift);
+    my $entries = shift;
+    foreach my $entry ( @$entries ) {  $class->find_or_create( $entry ); }
+}
+
