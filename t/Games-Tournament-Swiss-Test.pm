189c685eda8478ec9950ad411bbcfb3620b3d1d4
drbean <drbean@users.noreply.github.com>
2011-03-05 13:12:45 +0000

    Card, not Match

--- t/Games-Tournament-Swiss-Test.pm
+++ t/Games-Tournament-Swiss-Test.pm
@@ -12,7 +12,7 @@ our @EXPORT = qw/@p @g $gs $s $meets $play/;
 
 use Games::Tournament::Contestant::Swiss -base;
 use Games::Tournament::Swiss -base;
-use Games::Tournament::Match -base;
+use Games::Tournament::Card -base;
 
 our @p;
 $p[0] = Games::Tournament::Contestant::Swiss->new( id => 9430101, name => 'Roy', title => 'Expert', rating => 100,  );
@@ -27,23 +27,23 @@ $p[7] = Games::Tournament::Contestant::Swiss->new( id => 9430108, name => 'Red',
 our $s = Games::Tournament::Swiss->new(entrants => \@p);
 
 our @g;
-$g[0] = Games::Tournament::Match->new( round => 1, contestants => {Black => $p[7], White => $p[0]}, result => {Black => 'Loss'} );
-$g[1] = Games::Tournament::Match->new( round => 2, contestants => {Black => $p[2], White => $p[0]}, result => {Black => 'Loss'} );
-$g[2] = Games::Tournament::Match->new( round => 3, contestants => {Black => $p[4], White => $p[0]}, result => {Black => 'Loss'} );
+$g[0] = Games::Tournament::Card->new( round => 1, contestants => {Black => $p[7], White => $p[0]}, result => {Black => 'Loss'} );
+$g[1] = Games::Tournament::Card->new( round => 2, contestants => {Black => $p[2], White => $p[0]}, result => {Black => 'Loss'} );
+$g[2] = Games::Tournament::Card->new( round => 3, contestants => {Black => $p[4], White => $p[0]}, result => {Black => 'Loss'} );
 
-$g[3] = Games::Tournament::Match->new( round => 1, contestants => {Black => $p[6], White => $p[1]}, result => {Black => 'Loss'} );
-$g[4] = Games::Tournament::Match->new( round => 2, contestants => {Black => $p[7], White => $p[1]}, result => {Black => 'Loss'} );
-$g[5] = Games::Tournament::Match->new( round => 3, contestants => {Black => $p[3], White => $p[1]}, result => {Black => 'Loss'} );
+$g[3] = Games::Tournament::Card->new( round => 1, contestants => {Black => $p[6], White => $p[1]}, result => {Black => 'Loss'} );
+$g[4] = Games::Tournament::Card->new( round => 2, contestants => {Black => $p[7], White => $p[1]}, result => {Black => 'Loss'} );
+$g[5] = Games::Tournament::Card->new( round => 3, contestants => {Black => $p[3], White => $p[1]}, result => {Black => 'Loss'} );
 
-$g[6] = Games::Tournament::Match->new( round => 1, contestants => {Black => $p[5], White => $p[2]}, result => {Black => 'Loss'} );
-$g[7] = Games::Tournament::Match->new( round => 3, contestants => {Black => $p[7], White => $p[2]}, result => {Black => 'Loss'} );
+$g[6] = Games::Tournament::Card->new( round => 1, contestants => {Black => $p[5], White => $p[2]}, result => {Black => 'Loss'} );
+$g[7] = Games::Tournament::Card->new( round => 3, contestants => {Black => $p[7], White => $p[2]}, result => {Black => 'Loss'} );
 
-$g[8] = Games::Tournament::Match->new( round => 1, contestants => {Black => $p[4], White => $p[3]}, result => {Black => 'Loss'} );
-$g[9] = Games::Tournament::Match->new( round => 2, contestants => {Black => $p[6], White => $p[3]}, result => {Black => 'Loss'} );
+$g[8] = Games::Tournament::Card->new( round => 1, contestants => {Black => $p[4], White => $p[3]}, result => {Black => 'Loss'} );
+$g[9] = Games::Tournament::Card->new( round => 2, contestants => {Black => $p[6], White => $p[3]}, result => {Black => 'Loss'} );
 
-$g[10] = Games::Tournament::Match->new( round => 2, contestants => {Black => $p[5], White => $p[4]}, result => {Black => 'Loss'} );
+$g[10] = Games::Tournament::Card->new( round => 2, contestants => {Black => $p[5], White => $p[4]}, result => {Black => 'Loss'} );
 
-$g[11] = Games::Tournament::Match->new( round => 3, contestants => {Black => $p[6], White => $p[5]}, result => {Black => 'Loss'} );
+$g[11] = Games::Tournament::Card->new( round => 3, contestants => {Black => $p[6], White => $p[5]}, result => {Black => 'Loss'} );
 
 $s->assignPairingNumbers;
 map { $_->writeCard(@g) } @p;
