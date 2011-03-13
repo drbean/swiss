71dc5dfc1aafdd69681279134c4c5e895ad771ee
drbean <drbean@users.noreply.github.com>
2011-03-13 13:29:44 +0000

    Generate round.yaml from swiss db info

---
+++ web/script_files/create_round.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl 
+
+# Created: 西元2011年03月01日 13時36分22秒
+# Last Edit: 2011  3月 13, 21時32分31秒
+# $Id$
+
+=head1 NAME
+
+create_round.pl - Generate a round.yaml file representing a CompComp round
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+create_round.pl -l FLA0030 -r 2 > ~drbean/class/FLA0030/comp/2/round.yaml
+
+=cut
+
+use strict;
+use warnings;
+use IO::All;
+use YAML qw/LoadFile DumpFile Dump Bless/;
+use List::Util qw/sum/;
+use Cwd; use File::Basename;
+use Grades;
+
+=head1 DESCRIPTION
+
+Data from swiss database and from compcomp exercise content files are collated to create a list of members, and their tables and exercises etc, which can be munged, depending on the actual tables played, and the database updated with round_table.pl.
+
+=cut
+
+# use lib '../../swiss/web/lib';
+use lib '/var/www/cgi-bin/swiss/lib';
+use Swiss;
+use Swiss::Model::DB;
+use Swiss::Schema;
+
+my $script = Grades::Script->new_with_options;
+my $id = $script->league || basename( getcwd );
+my $round = $script->round;
+my $topic = $script->exercise;
+my $form = 0;
+
+my $leagues = '/home/drbean/992';
+
+my $league = League->new( leagues => $leagues, id => $id );
+my $g = Compcomp->new( league => $league );
+my $members = $league->members;
+my %members = map { $_->{id} => $_ } @$members;
+my @roles = qw/White Black/;
+
+my $connect_info = Swiss::Model::DB->config->{connect_info};
+my $d = Swiss::Schema->connect( @$connect_info );
+
+my $foundround = $d->resultset('Round')->find( { tournament => $id } )
+                ->value;
+my $m = $d->resultset('Matches')->search({ tournament => $id,
+		round => $round });
+
+my (%round, $players);
+my $n = 0;
+while ( my $match = $m->find({ pair => $n }) )  {
+	my $pair = { White => $match->white, Black => $match->black };
+	if ( $pair->{Black} eq 'Bye' ) {
+	    $round{bye} = $pair->{White};
+	    $n++;
+	}
+	else {
+	    Bless( $pair )->keys( [ qw/White Black/ ] );
+	    $players->{ $n++ } = $pair;
+	}
+}
+
+my @tables = sort {$a<=>$b} keys %$players;
+Bless( $players )->keys([ @tables ]); 
+
+$round{group} = $players;
+$round{round} = $round;
+$round{late} = [undef];
+$round{week} = undef;
+$round{text} = undef;
+$round{activity}->{$topic}->{$form} = \@tables;
+
+print Dump \%round;
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of create_round.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
