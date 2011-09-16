c4f6e69b5051f2d86d2d8a63f90e5ca94850242e
drbean <drbean@users.noreply.github.com>
2011-09-16 06:26:47 +0000

    001

--- web/script_files/create_round.pl
+++ web/script_files/create_round.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 
 # Created: 西元2011年03月01日 13時36分22秒
-# Last Edit: 2011  5月 22, 16時31分24秒
+# Last Edit: 2011 Sep 16, 02:18:57 PM
 # $Id$
 
 =head1 NAME
@@ -51,7 +51,7 @@ my $round = $script->round;
 my $topic = $script->exercise;
 my $form = 0;
 
-my $leagues = '/home/drbean/992';
+my $leagues = '/home/drbean/001';
 ( my $leagueid = $id ) =~ s/^([[:alpha:]]+[[:digit:]]+).*$/$1/;
 
 my $league = League->new( leagues => $leagues, id => $leagueid );
