299e6604dcc9d839fa7702732901de386df72abb
drbean <drbean@users.noreply.github.com>
2012-10-07 12:57:21 +0000

    011,substr_league.name

--- web/script_files/populate.pl
+++ web/script_files/populate.pl
@@ -60,12 +60,12 @@ find_or_populate( 'Arbiters', \@officials );
 my $roundset = $d->resultset('Round');
 my (@startingrounds, %players, @members, @ratings, @scores);
 for my $tournament (
-	qw/FIA0034 BMA0059 MIA0013 GL00007 GL00034 FLA0010 FLA0018 FLA0010-2 FLA0018-2/
+	qw/FIA0033 BMA0056 BMA0058 MIA0014 FLA0016 00034522 00034519/
 	) {
 	( my $id = $tournament ) =~ s/^([[:alpha:]]+[[:digit:]]+).*$/$1/;
 	my $league = League->new( leagues => $config{leagues}, id => $id );
 	my $members = $league->members;
-	my $name = $league->name;
+	my $name = substr $league->name, 0, 14;
 	my $description = $league->field;
 	my $arbiter = '193001';
 	my $rounds = 18;
