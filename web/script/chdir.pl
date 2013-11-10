a9507e926602a40db5a04bfa20471c4c80d4bf4a
drbean <drbean@users.noreply.github.com>
2013-11-10 08:26:05 +0000

    Attempting to get FindBin to find Swiss.pm rather than hard-code it with use lib

---
+++ web/script/chdir.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl 
+
+# Created: 11/10/2013 04:05:43 PM
+# Last Edit: 2013 Nov 10, 04:10:05 PM
+# $Id$
+
+=head1 NAME
+
+chdir.pl - FindBin will find in this file's dir rather than master script
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+Trying to get update_db not to set $Bin to /usr/local/bin, but dir of required modules in /home/drbean/swiss
+
+=cut
+
+use FindBin qw/$Bin/;
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+chdir '/home/drbean/swiss/web/script/';
+
+sub run {
+    1;
+}
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of chdir.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
+
