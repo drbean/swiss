ff9006d2c62cb2ee3221aea89a623ba3e78e399b
drbean <drbean@users.noreply.github.com>
2014-03-24 07:55:56 +0000

    022

--- web/lib/Swiss/Model/DB.pm
+++ web/lib/Swiss/Model/DB.pm
@@ -7,7 +7,7 @@ use Catalyst;
 use Swiss;
 
 my $name = Swiss->config->{database};
-# my $name = 'swiss021';
+# my $name = 'swiss022';
 
 my $connect_info;
 if ( $^O eq 'linux' ) { $connect_info = [ "dbi:Pg:dbname=$name", '', '', ]; }
