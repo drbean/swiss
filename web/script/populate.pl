232ca30c375e790c14b09e1e81c1f2885616ab3b
drbean <drbean@freeshell.org>
2018-03-20 19:29:20 +0800

    062

--- web/script/populate.pl
+++ web/script/populate.pl
@@ -57,7 +57,7 @@ find_or_populate( 'Arbiters', \@officials );
 my $roundset = $d->resultset('Round');
 my (@startingrounds, %players, @members, @ratings, @scores);
 for my $tournament (
-    qw/2L1a 2L1b/
+    qw/2L2/
 	) {
 	# ( my $id = $tournament ) =~ s/^([[:alpha:]]+[[:digit:]]+).*$/$1/;
 	my $id = $tournament;
